62941186d9f4dd027fb5a7a729847e8eea048f88
George Darling <devgr4@gmail.com>
2018-01-27 11:31:30 -0600

    added player sprite and set the this variable to be used for game objects

--- js/game.js
+++ js/game.js
@@ -1,21 +1,23 @@
 window.onload = function() {
 	window.require(['preload', 'create', 'update', 'render'], function(preload, create, update, render) {
+		var gameObjects = {};
 		function preloader() {
-			preload(game);
+			preload.call(gameObjects, game);
 		}
 
 		function creater() {
-			create(game);
+			create.call(gameObjects, game);
 		}
 
 		function updater() {
-			update(game);
+			update.call(gameObjects, game);
 		}
 
 		function renderer() {
-			render(game);
+			render.call(gameObjects, game);
 			
 		}
-		var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preloader, create: creater, update: updater, render: renderer });
+		var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preloader, create: creater, update: updater, render: renderer }, false, false);
+		Phaser.Canvas.setSmoothingEnabled(game.context, false);
 	});
 };
--- js/update.js
+++ js/update.js
@@ -1,3 +1,2 @@
 window.export('update', function(game){
-
 });
